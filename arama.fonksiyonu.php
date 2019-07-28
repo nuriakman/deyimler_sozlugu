40e0cef95bf8b73ad62490aedb8d57d3b2679ea7
Nuri Akman <nuriakman@gmail.com>
2019-07-28 15:55:10 +0300

    Oluşabilecek Türkçe karakter gösterimi sorunlarını giderme için set names eklendi

--- arama.fonksiyonu.php
+++ arama.fonksiyonu.php
@@ -9,6 +9,9 @@ $db = mysqli_connect($servername, $username, $password, $dbname);
 // Varsa, bağlantı hatasının ekrana yazdırılarak programın sonlandırılması
 if (!$db) { die("Hata oluştu: " . mysqli_connect_error()); }
 //echo "Bağlantı tamam!";
+
+// Oluşabilecek Türkçe karakter gösterimi sorunlarını giderelim...
+mysqli_query($db, "set names 'utf8'");
 ?>
 
 <div class="alert alert-success" role="alert">
