2131c0a692e8b347ca64dd9afe7b2f672d673800
Nuri Akman <nuriakman@gmail.com>
2019-07-28 12:34:14 +0300

    Proje hakkında sayfası eklendi.

---
+++ hakkimizda.php
@@ -0,0 +1,85 @@
+<!doctype html>
+<html lang="tr">
+  <head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+
+    <title>Bulmaca Sözlüğü</title>
+  </head>
+  <body>
+
+
+    <div class="container">
+
+      <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+        <a class="navbar-brand" href="index.php">BulmacaSözlüğü</a>
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+          <span class="navbar-toggler-icon"></span>
+        </button>
+
+        <div class="collapse navbar-collapse" id="navbarSupportedContent">
+          <ul class="navbar-nav mr-auto">
+            <li class="nav-item active">
+              <a class="nav-link" href="index.php">Ana Sayfa <span class="sr-only">(current)</span></a>
+            </li>
+            <li class="nav-item active">
+              <a class="nav-link" href="hakkimizda.php">Hakkımızda <span class="sr-only">(current)</span></a>
+            </li>
+          </ul>
+        </div>
+      </nav>
+
+<div class="row mt-4 mb-4">
+  <div class="col-md-12 text-center">
+      <img class="img-fluid" src="bulmaca.png" />
+  </div>
+</div>
+
+<div class="row">
+  <div class="col-md-2">
+
+  </div>
+  <div class="col-md-8">
+    <div class="jumbotron">
+      <h1 class="display-4">Proje Hakkında...</h1>
+      <p class="lead">Bu proje, BootStrap, PHP ve MySQL'i anlamak için Özgür Yazılım Yaz Kampında örnek olarak geliştirilmiştir.</p>
+      <hr class="my-4">
+      <p>Özgür Yazılım Yaz Kampı hakkında detaylı bilgiler</p>
+      <a class="btn btn-success btn-lg" href="https://kamp.linux.org.tr/2019/yaz/" role="button">Özgür Yazılım Yaz Kampı</a>
+    </div>
+  </div>
+  <div class="col-md-2">
+
+  </div>
+</div> <!-- row -->
+
+<div class='row'>
+  <div class='col-md-12'>
+    <div class="alert alert-dark text-center" role="alert">
+      Özgür Yazılım Yaz Kampı, PHP Sınıfı, 2019
+    </div>
+  </div>
+</div>
+
+
+
+
+    </div> <!-- Container -->
+
+
+
+
+
+
+
+    <!-- Optional JavaScript -->
+    <!-- jQuery first, then Popper.js, then Bootstrap JS -->
+    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
+  </body>
+</html>
